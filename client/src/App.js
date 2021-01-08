59a7899b3f6a51c27cf4445f1fe17b1bac212fbe
Ali Nguyen <alin57717@gmail.com>
2021-01-07 22:31:55 -0800

    set up full stack structure with client side react app

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql'
+  uri: '/graphql'
 });
 
 function App() {
